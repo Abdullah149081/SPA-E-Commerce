6d5de84d83c9a2626727eaf4088cc675e66d66b0
Abdullah149081 <masudmd425@gmail.com>
2023-05-16 17:50:21 +0600

    firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDfhohdp6OcHSXZlXKIGKobitnQLxxnQjE",
-  authDomain: "spa-e-commerce-7f32e.firebaseapp.com",
-  projectId: "spa-e-commerce-7f32e",
-  storageBucket: "spa-e-commerce-7f32e.appspot.com",
-  messagingSenderId: "128450840262",
-  appId: "1:128450840262:web:c250f9f0aaf1488329f9c7",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
